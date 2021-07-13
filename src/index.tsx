b94c64b53c1e2625b4e43ecb7b6889f733a30d03
Aditya V <mercisthebest98@gmail.com>
2021-07-13 12:19:03 +0530

    default redirect

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
@@ -11,6 +11,9 @@ ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
         <Switch>
+          <Route path="/" exact>
+            <Redirect to="/summary?filter=all" />
+          </Route>
           <Route path="/summary" component={App} exact />
         </Switch>
       </BrowserRouter>
